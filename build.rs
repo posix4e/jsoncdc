e7bdbec803ccf29f77680e8032ff4bff6b489e5a
Jason Dusek <jason.dusek@gmail.com>
2015-12-17 00:09:12 -0800

    Builds/works on Mac

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() {
-    println!("cargo:rustc-link-lib=static=pgcommon");
+    println!("cargo:rustc-link-search=native=/usr/local/opt/openssl/lib");
+    println!("cargo:rustc-link-lib=dylib=ssl");
     println!("cargo:rustc-link-search=native=/usr/local/lib/");
+    println!("cargo:rustc-link-lib=dylib=pq");
+    println!("cargo:rustc-link-lib=dylib=pgport");
+    println!("cargo:rustc-link-lib=dylib=pgcommon");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate elephantpump_sys;
 
 fn main() {
     unsafe {
-        println!("Hello World! {}", elephantpump_sys::wal_level);
+        println!("Hello World! {}", elephantpump_sys::h_errno);
     }
 }
 
