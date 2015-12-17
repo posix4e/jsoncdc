cdf801aa69e291d2943e80a65aa44282c7800e3a
Jason Dusek <jason.dusek@gmail.com>
2015-12-16 20:52:35 -0800

    Progress of a kind

--- elephantpump-sys/build.rs
+++ elephantpump-sys/build.rs
@@ -1,4 +1,8 @@
 fn main() {
-    println!("cargo:rustc-link-lib=static=pgcommon");
+    println!("cargo:rustc-link-search=native=/usr/local/opt/openssl/lib");
+    println!("cargo:rustc-link-lib=static=ssl");
     println!("cargo:rustc-link-search=native=/usr/local/lib/");
+    println!("cargo:rustc-link-lib=static=pq");
+    println!("cargo:rustc-link-lib=static=pgport");
+    println!("cargo:rustc-link-lib=static=pgcommon");
 }
