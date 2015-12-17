d41be44b3ae7ab66e8ad4e581ab0f54953147cf4
Jason Dusek <jason.dusek@gmail.com>
2015-12-17 01:01:03 -0800

    Try to bind just one function

--- elephantpump-sys/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native=/usr/local/opt/openssl/lib");
-    println!("cargo:rustc-link-lib=dylib=ssl");
-    println!("cargo:rustc-link-search=native=/usr/local/lib/");
-    println!("cargo:rustc-link-lib=dylib=pq");
-    println!("cargo:rustc-link-lib=dylib=pgport");
-    println!("cargo:rustc-link-lib=dylib=pgcommon");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-extern crate elephantpump_sys;
+extern crate libc;
+
+extern "C" {
+    pub fn pg_str_endswith(s1: *const ::libc::c_char,
+                           s2: *const ::libc::c_char) -> ::libc::c_int;
+}
 
 fn main() {
+    let a = std::ffi::CString::new("a").unwrap();
+    let b = std::ffi::CString::new("b").unwrap();
     unsafe {
-        println!("Hello World! {}", elephantpump_sys::h_errno);
+        println!("A ends with A: {}", pg_str_endswith(a.as_ptr(), a.as_ptr()));
+        println!("A ends with B: {}", pg_str_endswith(a.as_ptr(), b.as_ptr()));
     }
 }
 
