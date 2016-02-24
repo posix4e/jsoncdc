38efbf0b07ae2460215e4f3b481a80889713eddd
Jason Dusek <jason.dusek@gmail.com>
2016-02-23 23:14:32 -0800

    Rename `libpq` to `pg`
    It's not very much like the real `libpq`, as someone point out on #postgresql.

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,15 @@ use std::mem::size_of;
         non_snake_case,
         non_camel_case_types,
         non_upper_case_globals)]
-pub mod libpq;
+pub mod pg;
 
 
-const CTRUE: libpq::_bool = 1;
-const CFALSE: libpq::_bool = 0;
+const CTRUE: pg::_bool = 1;
+const CFALSE: pg::_bool = 0;
 
 // Implementation of initialization and callbacks.
 
-pub unsafe extern fn init(cb: *mut libpq::OutputPluginCallbacks) {
+pub unsafe extern fn init(cb: *mut pg::OutputPluginCallbacks) {
     (*cb).startup_cb = Some(startup);
     (*cb).begin_cb = Some(begin);
     (*cb).change_cb = Some(change);
@@ -23,83 +23,83 @@ pub unsafe extern fn init(cb: *mut libpq::OutputPluginCallbacks) {
 }
 
 #[allow(unused_variables)]
-extern fn startup(ctx: *mut libpq::Struct_LogicalDecodingContext,
-                  options: *mut libpq::OutputPluginOptions,
-                  is_init: libpq::_bool) {
+extern fn startup(ctx: *mut pg::Struct_LogicalDecodingContext,
+                  options: *mut pg::OutputPluginOptions,
+                  is_init: pg::_bool) {
     unsafe {
-        let last_relid = libpq::palloc0(size_of::<libpq::Oid>() as u64);
+        let last_relid = pg::palloc0(size_of::<pg::Oid>() as u64);
         (*ctx).output_plugin_private = last_relid;
-        (*options).output_type = libpq::OUTPUT_PLUGIN_TEXTUAL_OUTPUT;
+        (*options).output_type = pg::OUTPUT_PLUGIN_TEXTUAL_OUTPUT;
     }
 }
 
 #[allow(unused_variables)]
-extern fn begin(ctx: *mut libpq::Struct_LogicalDecodingContext,
-                txn: *mut libpq::ReorderBufferTXN) {
+extern fn begin(ctx: *mut pg::Struct_LogicalDecodingContext,
+                txn: *mut pg::ReorderBufferTXN) {
     unsafe {
         let s = CString::new("{ \"begin\": %u }").unwrap();
-        libpq::OutputPluginPrepareWrite(ctx, CTRUE);
-        libpq::appendStringInfo((*ctx).out, s.as_ptr(), (*txn).xid);
-        libpq::OutputPluginWrite(ctx, CTRUE);
+        pg::OutputPluginPrepareWrite(ctx, CTRUE);
+        pg::appendStringInfo((*ctx).out, s.as_ptr(), (*txn).xid);
+        pg::OutputPluginWrite(ctx, CTRUE);
     }
 }
 
 #[allow(unused_variables)]
-extern fn change(ctx: *mut libpq::Struct_LogicalDecodingContext,
-                 txn: *mut libpq::ReorderBufferTXN,
-                 relation: libpq::Relation,
-                 change: *mut libpq::ReorderBufferChange) {
+extern fn change(ctx: *mut pg::Struct_LogicalDecodingContext,
+                 txn: *mut pg::ReorderBufferTXN,
+                 relation: pg::Relation,
+                 change: *mut pg::ReorderBufferChange) {
     unsafe {
         let relid = (*relation).rd_id;
-        let last_relid: *mut libpq::Oid =
-            (*ctx).output_plugin_private as *mut libpq::Oid;
+        let last_relid: *mut pg::Oid =
+            (*ctx).output_plugin_private as *mut pg::Oid;
         if *last_relid != relid {
-            libpq::OutputPluginPrepareWrite(ctx, CFALSE);
+            pg::OutputPluginPrepareWrite(ctx, CFALSE);
             append_schema(relation, (*ctx).out);
-            libpq::OutputPluginWrite(ctx, CFALSE);
+            pg::OutputPluginWrite(ctx, CFALSE);
             *last_relid = relid;
         }
-        libpq::OutputPluginPrepareWrite(ctx, CTRUE);
+        pg::OutputPluginPrepareWrite(ctx, CTRUE);
         append_change(relation, change, (*ctx).out);
-        libpq::OutputPluginWrite(ctx, CTRUE);
+        pg::OutputPluginWrite(ctx, CTRUE);
     }
 }
 
 #[allow(unused_variables)]
-extern fn commit(ctx: *mut libpq::Struct_LogicalDecodingContext,
-                 txn: *mut libpq::ReorderBufferTXN,
-                 lsn: libpq::XLogRecPtr) {
+extern fn commit(ctx: *mut pg::Struct_LogicalDecodingContext,
+                 txn: *mut pg::ReorderBufferTXN,
+                 lsn: pg::XLogRecPtr) {
     unsafe {
         let s = CString::new("{ \"commit\": %u, \"t\": \"%s\" }").unwrap();
-        let t = libpq::timestamptz_to_str((*txn).commit_time);
-        libpq::OutputPluginPrepareWrite(ctx, CTRUE);
-        libpq::appendStringInfo((*ctx).out, s.as_ptr(), (*txn).xid, t);
-        libpq::OutputPluginWrite(ctx, CTRUE);
-        let last_relid: *mut libpq::Oid =
-            (*ctx).output_plugin_private as *mut libpq::Oid;
+        let t = pg::timestamptz_to_str((*txn).commit_time);
+        pg::OutputPluginPrepareWrite(ctx, CTRUE);
+        pg::appendStringInfo((*ctx).out, s.as_ptr(), (*txn).xid, t);
+        pg::OutputPluginWrite(ctx, CTRUE);
+        let last_relid: *mut pg::Oid =
+            (*ctx).output_plugin_private as *mut pg::Oid;
         *last_relid = 0;
     }
 }
 
 #[allow(unused_variables)]
-extern fn shutdown(ctx: *mut libpq::Struct_LogicalDecodingContext) {
+extern fn shutdown(ctx: *mut pg::Struct_LogicalDecodingContext) {
     unsafe {
-        libpq::pfree((*ctx).output_plugin_private);
+        pg::pfree((*ctx).output_plugin_private);
     }
 }
 
 
-unsafe fn append_change(relation: libpq::Relation,
-                        change: *mut libpq::ReorderBufferChange,
-                        out: libpq::StringInfo) {
+unsafe fn append_change(relation: pg::Relation,
+                        change: *mut pg::ReorderBufferChange,
+                        out: pg::StringInfo) {
     let tuple_desc = (*relation).rd_att;
     let tuples = (*change).data.tp();
     let tuple_new = (*tuples).newtuple;
     let tuple_old = (*tuples).oldtuple;
     let token = match (*change).action {
-        libpq::REORDER_BUFFER_CHANGE_INSERT => "insert",
-        libpq::REORDER_BUFFER_CHANGE_UPDATE => "update",
-        libpq::REORDER_BUFFER_CHANGE_DELETE => "delete",
+        pg::REORDER_BUFFER_CHANGE_INSERT => "insert",
+        pg::REORDER_BUFFER_CHANGE_UPDATE => "update",
+        pg::REORDER_BUFFER_CHANGE_DELETE => "delete",
         _ => panic!("Unrecognized change action!")
     };
     append("{ ", out);
@@ -114,37 +114,37 @@ unsafe fn append_change(relation: libpq::Relation,
     append(" }", out);
 }
 
-unsafe fn append_tuple_buf_as_json(data: *mut libpq::ReorderBufferTupleBuf,
-                                   desc: libpq::TupleDesc,
-                                   out: libpq::StringInfo) {
+unsafe fn append_tuple_buf_as_json(data: *mut pg::ReorderBufferTupleBuf,
+                                   desc: pg::TupleDesc,
+                                   out: pg::StringInfo) {
     if !data.is_null() {
         let heap_tuple = &mut (*data).tuple;
-        let datum = libpq::heap_copy_tuple_as_datum(heap_tuple, desc);
-        let empty_oid: libpq::Oid = 0;
-        let json = libpq::DirectFunctionCall1Coll(Some(row_to_json),
+        let datum = pg::heap_copy_tuple_as_datum(heap_tuple, desc);
+        let empty_oid: pg::Oid = 0;
+        let json = pg::DirectFunctionCall1Coll(Some(row_to_json),
                                                   empty_oid,
                                                   datum);
-        let ptr = json as *const libpq::Struct_varlena;
-        let text = libpq::text_to_cstring(ptr);
-        libpq::appendStringInfoString(out, text);
+        let ptr = json as *const pg::Struct_varlena;
+        let text = pg::text_to_cstring(ptr);
+        pg::appendStringInfoString(out, text);
     } else {
         append("{}", out);
     }
 }
 
-unsafe fn append<T: Into<Vec<u8>>>(t: T, out: libpq::StringInfo) {
-    libpq::appendStringInfoString(out, CString::new(t).unwrap().as_ptr());
+unsafe fn append<T: Into<Vec<u8>>>(t: T, out: pg::StringInfo) {
+    pg::appendStringInfoString(out, CString::new(t).unwrap().as_ptr());
 }
 
-unsafe fn append_schema(relation: libpq::Relation, out: libpq::StringInfo) {
+unsafe fn append_schema(relation: pg::Relation, out: pg::StringInfo) {
     let relid = (*relation).rd_id;
     let tupdesc = (*relation).rd_att;
-    let name = libpq::get_rel_name(relid);
-    let ns = libpq::get_namespace_name(libpq::get_rel_namespace(relid));
-    let qualified_name = libpq::quote_qualified_identifier(ns, name);
+    let name = pg::get_rel_name(relid);
+    let ns = pg::get_namespace_name(pg::get_rel_namespace(relid));
+    let qualified_name = pg::quote_qualified_identifier(ns, name);
     append("{ \"table\": ", out);
     append("\"", out);
-    libpq::appendStringInfoString(out, qualified_name);
+    pg::appendStringInfoString(out, qualified_name);
     append("\"", out);
     append(",", out);
     append(" \"schema\": ", out);
@@ -157,23 +157,23 @@ unsafe fn append_schema(relation: libpq::Relation, out: libpq::StringInfo) {
         if (*attr).attisdropped == 1 || num <= 0 {
             continue;
         }
-        let col = libpq::get_attname(relid, num);
-        let typ = libpq::format_type_be(libpq::get_atttype(relid, num));
+        let col = pg::get_attname(relid, num);
+        let typ = pg::format_type_be(pg::get_atttype(relid, num));
         if !first {
             append(",", out);
         } else {
             first = false;
         }
-        libpq::appendStringInfo(out, fmt.as_ptr(), col, typ);
+        pg::appendStringInfo(out, fmt.as_ptr(), col, typ);
     }
     append("]", out);
     append(" }", out);
 }
 
-extern fn row_to_json(fcinfo: libpq::FunctionCallInfo) -> libpq::Datum {
+extern fn row_to_json(fcinfo: pg::FunctionCallInfo) -> pg::Datum {
     // We wrap the unsafe call to make it safe.
     unsafe {
-        libpq::row_to_json(fcinfo)
+        pg::row_to_json(fcinfo)
     }
 }
 
@@ -187,4 +187,4 @@ pub unsafe extern fn _PG_init() { }
 #[allow(non_snake_case)]
 #[no_mangle]
 pub unsafe extern fn
-    _PG_output_plugin_init(cb: *mut libpq::OutputPluginCallbacks) { init(cb); }
+    _PG_output_plugin_init(cb: *mut pg::OutputPluginCallbacks) { init(cb); }
